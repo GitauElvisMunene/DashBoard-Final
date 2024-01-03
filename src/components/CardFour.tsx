f5bf71b2a35e4cd08f7cbc001a4403231aa155da
Elvis-Munene <elvismuneneg@gmail.com>
2024-01-03 14:53:37 +0300

    matches the Liquid Quantity Readings

--- src/components/CardFour.tsx
+++ src/components/CardFour.tsx
@@ -41,7 +41,7 @@ const CardFour: React.FC<ChartFourProps> = ({ data }) => {
         <div className="mt-4 flex items-end justify-between">
           <div>
             <h4 className="text-title-md font-bold text-black dark:text-white">
-            {latestData.device_data.flowVolume} Litres
+            {latestData.device_data.OutputLiquidQuantity} Litres
             </h4>
             <span className="text-sm font-medium">Liquid Output</span>
           </div>
